17cf679165c0d7289bfe02ea96e14e8f1aa9eb73
sdhruvan <90624304+sdhruvan@users.noreply.github.com>
2023-01-19 19:17:16 +0530

    Add files via upload

---
+++ PRO-C105-Project-Images-main/PRO-C105-Project-Images-main/pro117.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width,height)
+
+output = cv2.VideoWriter('album.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 3, size)
+
+for i in range(count-1,0,-1):
+    frame = cv2.imread(images[i])
+    output.write(frame)
+
+#for sunset
+#for i in range(0,count-1,1):
+ #   frame = cv2.imread(images[i])
+  #  output.write(frame)
+
+
+output.release()
+print("Done")
